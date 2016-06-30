1ee4636fc0f605e63cd8e93dd8bb60c4b2ba9507
Karl Lapo <lapo.karl@gmail.com>
2016-06-30 16:17:41 -0700

    Added try-except statement for selecting package building tool

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from setuptools import setup
+import os
+import re
+import sys
+import warnings
+try:
+    from setuptools import setup
+    from setuptools.extension import Extension
+except:
+    from distutils.core import setup
+    from distutils.extension import Extension
 
 setup(name='turbpy',
    author="Karl Lapo",
    author_email="lapok@atmos.washington.edu",
    description="Turbulence related functions for land surface models",
-   version='1',
+   version='0.1',
    packages=["turbpy"])
