96dce9306749477917e4fdfbfb615e1b2e26535c
Karl Lapo <lapo.karl@gmail.com>
2016-07-15 10:14:13 -0700

    .gitignore is now working

--- build/lib/turbpy/__init__.py
+++
@@ -1,6 +0,0 @@
-# __all__ = ["multiConst","conversion","bulkRichardson","aStability","aeroResist","turbfluxes"]
-from conversionTools import *
-from bulkRichardson import bulkRichardson
-from aStability import aStability
-from aeroResist import aeroResist
-from turbFluxes import turbFluxes
