Fixed moninObukhov: 	- vapor pressures now in correct units (hPa) 	- changed np.max to max, to get desired behavior 	- Now pass wind coefficients to surfFluxCalc.moninObukhov. Windless 	  exchange parameter now behaves as expected 	- Tested moninObukhov and verified that it matches SNTHERM output

turbpy/aStability.py
@@ -166,7 +166,7 @@ def aStability(computeDerivative, ixStability, ixStabParam, mHeight, airTemp,
         psiH = 0.					# Stability function for heat
         psiQ = 0.					# Stability function for water vapor
         deltaT = airTemp - sfcTemp
-        deltaE = airVaporPress - sfcVaporPress
+        deltaE = (airVaporPress - sfcVaporPress) / 100.  # Convert [Pa] -> [hPa]
         Tbar = (airTemp + sfcTemp) / 2.
         zetaT = RiBulk * dlogW      # Stability parameter
         L = mHeight / zetaT 		# Obukhov height
@@ -206,7 +206,7 @@ def aStability(computeDerivative, ixStability, ixStabParam, mHeight, airTemp,
             else:
                 # Unstable
                 psiM = Unstable(L, mHeight, z0Groundh, 1)
-            sqrtCd = mc.vkc / np.max((dlogW - psiM), 0.1)
+            sqrtCd = mc.vkc / max((dlogW - psiM), 0.1)
             ustar = sqrtCd * windspd
 
             #########
@@ -229,7 +229,7 @@ def aStability(computeDerivative, ixStability, ixStabParam, mHeight, airTemp,
                 psiQ = psiH
             # Scalar lengths for temperature and moisture
             tstar = mc.vkc * deltaT / (dlogT - psiH)
-            qstar = (mc.vkc * deltaE
+            qstar = (mc.vkc * deltaE * 100
                      / (mc.R_wv * airTemp * mc.iden_air)
                      / (dlogQ - psiQ))
 

turbpy/surfFluxCalc.py
@@ -23,9 +23,9 @@ def moninObukhov(airTemp, airVaporPress, sfcTemp, sfcVaporPress,
     latHeatGround = Ck + latHeatGround0
     if L >= 0:
         senHeatGround = senHeatGround0
-        if senHeatGround0 < np.max(Csk, .1) and senHeatGround0 > 0:
+        if senHeatGround0 < max(Csk, .1) and senHeatGround0 > 0:
             # Enforce a minimum value.
-            senHeatGround = np.max(Csk, .1)
+            senHeatGround = max(Csk, .1)
     else:
         # Sensible heat maximum for unstable
         dum = 0
@@ -37,6 +37,9 @@ def moninObukhov(airTemp, airVaporPress, sfcTemp, sfcVaporPress,
         dum = 0
         if not deltaE == 0:
             dum = -freelimv / deltaE
-        latHeatGround = np.max(latHeatGround, dum)
+        latHeatGround = max(latHeatGround, dum)
 
+    # Convert to fluxes
+    senHeatGround = senHeatGround * deltaT
+    latHeatGround = latHeatGround * deltaE
     return (senHeatGround, latHeatGround)