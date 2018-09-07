879d19ba191b4aaae7d22420029ce79f9775151b
Karl Lapo <karl.lapo@uni-bayreuth.de>
2018-09-07 11:07:45 +0200

    Forced check for temperature in Kelvin (after making some dumb mistakes :p

--- turbpy/bulkRichardson.py
+++ turbpy/bulkRichardson.py
@@ -1,5 +1,5 @@
 import turbpy.multiConst as mc
-
+import numpy as np
 
 def bulkRichardson(airTemp,                  # air temperature (K)
                    sfcTemp,                  # surface temperature (K)
@@ -8,6 +8,11 @@ def bulkRichardson(airTemp,                  # air temperature (K)
                    ):
     # -------------------------------------------------------------------------------------------------------
     # Local variables
+    if np.max(airTemp) < 200:
+        airTemp = airTemp + 273.15
+    if np.max(sfcTemp) < 200:
+        sfcTemp = sfcTemp + 273.15
+
     T_grad = airTemp - sfcTemp
     T_mean = 0.5 * (airTemp + sfcTemp)
     RiMult = (mc.gravity * mHeight) / (windspd**2.)
